0e1bd8faa6f4167e01994444a470ce5560cdf412
trevor-isiche <isiche.trevor@gmail.com>
2019-12-09 11:27:14 +0300

    fixed errors

--- Github-search-angular/src/app/app.module.ts
+++ Github-search-angular/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterComponent } from './master/master.component';
+import { ApiService } from './api-service/api.service';
+import { RepoComponent } from './repo/repo.component';
+import { RoutingComponent } from '../app/routing/routing.component';
+import { HighlightDirective } from './highlight.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent,
+    RepoComponent,
+    HighlightDirective,
+    
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RoutingComponent,
+
+
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
