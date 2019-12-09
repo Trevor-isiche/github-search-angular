99aa08537e3c32b65ec3fabf1a4112ffc3d6519a
trevor-isiche <isiche.trevor@gmail.com>
2019-12-09 12:16:46 +0300

    add import apiservice

--- Github-search-angular/src/app/repo/repo.component.ts
+++ Github-search-angular/src/app/repo/repo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api-service/api.service';
 
 @Component({
   selector: 'app-repo',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepoComponent implements OnInit {
 
-  constructor() { }
+  user1;
+  repos;
+  userName: string;
+
+  constructor(public apiservice: ApiService) { }
 
   ngOnInit() {
   }
