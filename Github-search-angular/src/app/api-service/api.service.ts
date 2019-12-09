9a4ea577c57443bf9d967332c6ae319c22c64795
trevor-isiche <isiche.trevor@gmail.com>
2019-12-09 14:39:21 +0300

    fix errors

--- Github-search-angular/src/app/api-service/api.service.ts
+++ Github-search-angular/src/app/api-service/api.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment} from 
+import { environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  userName: string;
+  apiUrl: string = "https://api.github.com/users/";
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    this.userName = "trevor-isiche"
+   }
+   getUserdetails() {
+     return this.http.get(this.apiUrl + this.userName + "?access_token=" + environment.apiKey)
+   }
+   getRepos() {
+     return this.http.get(this.apiUrl + this.userName + "/repos?access_token=" + environment.apiKey)
+   }
+  updateuserName(userName: string ) {
+    this.userName = userName;
+    
+  }
 }
